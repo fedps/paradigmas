f831156adedd656fcdf7ab563c052bd4fcd37483
fedps <felipe.souza@ecomp.ufsm.br>
2017-03-18 15:02:48 -0300

    Add files via upload

---
+++ t1/t1parte1.py
@@ -0,0 +1,65 @@
+#1. Defina uma função somaQuad(x,y) que calcule a soma dos quadrados de dois números x e y.
+def somaQuad(x,y):
+	return x**2+y**2
+
+#2. Crie uma função hasEqHeads(l1,l2) que verifique se as listas l1 e l2 possuem o mesmo primeiro elemento.
+def hasEqHeads(l1,l2):
+	return l1[0] == l2 [0]
+
+#3. Escreva uma função que receba uma lista de nomes e retorne uma lista com a string "Sr. " adicionada 
+#   ao início de cada nome. Defina uma função auxiliar para ajudar neste exercício.
+def auxSr(l):
+	return "Sr. "+l
+def addSr(l1):
+	return list(map(auxSr, l1))
+
+#4. Crie uma função que receba uma string e retorne o número de espaços nela contidos. Defina uma função auxiliar para ajudar neste exercício.
+def auxSpc(s):
+	return s == " "
+def countSpc(string):
+	return len(list(filter(auxSpc,string)))
+
+#5. Escreva uma função que, dada uma lista de números, calcule 3n*2 + 2/n + 1 para cada número n da lista. 
+#   Defina uma função auxiliar para ajudar neste exercício.
+def auxCalc(n):
+	return 3*n*2+2/n+1
+def listCalc(l):
+	return list(map(auxCalc,l))
+
+#6. Escreva uma função que, dada uma lista de números, retorne uma lista com apenas os que forem negativos. 
+#   Defina uma função auxiliar para ajudar neste exercício.
+def auxNeg(n):
+	return n < 0
+def listNeg(l):
+	return list(filter(auxNeg,l))
+
+#7. Escreva uma função que receba uma lista de números e retorne somente os que estiverem entre 1 e 100, inclusive. 
+#   Defina uma função auxiliar para ajudar neste exercício.
+def auxNum(n):
+	return n >= 1 and n<=100
+def listNum(l):
+	return list(filter(auxNum,l))
+
+#8. Escreva uma função que receba uma lista de números e retorne somente aqueles que forem pares. 
+#   Defina uma função auxiliar para ajudar neste exercício.
+def auxIsEven(x):
+	return x%2 == 0
+def isEven(l):
+	return list(filter(auxIsEven,l))
+
+#9. Crie uma função charFound(c,s) que verifique se o caracter c está contido na string. O resultado deve ser True ou False. 
+#   Você não deve usar o operador in. Defina uma função auxiliar para ajudar neste exercício.
+def auxChar(c,s):
+	return s.find(c)>=0
+
+def charFound(c,s):
+	return auxChar(c,s)
+
+#10. Escreva uma função que receba uma lista de strings e retorne uma nova lista com adição de marcações HTML (p.ex.: <B> e </B>) 
+#    antes e depois de cada string.
+def auxHtml(s):
+	return "<i>"+s+"</i>"
+def addHtml(l1):
+	return list(map(auxHtml, l1))
+
+
---
+++ t1/t1parte2.py
@@ -0,0 +1,30 @@
+#1. Escreva uma função que receba uma lista de nomes e retorne uma lista com a string "Sr. " adicionada ao início de cada nome.
+def addSr(l1):
+	return list(map(lambda l: "Sr. "+l, l1))
+
+#2. Escreva uma função que, dada uma lista de números, calcule 3n*2 + 2/n + 1 para cada número n da lista.
+def listCalc(l):
+	return list(map(lambda n:3*n*2+2/n+1 ,l))
+
+#3. Crie uma função que receba uma lista de nomes e retorne outra lista com somente aqueles nomes que terminarem com a letra 'a'.
+def endA(l):
+	return list(filter(lambda l1:'a'==l1[-1],l))
+
+#4. Escreva uma função que, dada uma lista de idades de pessoas no ano atual, retorne uma lista somente com as idades de quem nasceu depois de 1970. 
+#   Para testar a condição, sua função deverá subtrair a idade do ano atual. Exemplo de uso:
+#   
+#   >>> idades([20,30,51,57])
+#       [20, 30]
+def year70(l):
+	return list (filter(lambda l1: 2016-l1 >1970,l))
+
+#5. O código abaixo é escrito em Python imperativo. Escreva um código equivalente usando programação funcional.
+#
+#   numbers = [1, 2, 3, 4]
+#   new_numbers = []
+#   for number in numbers:
+#   new_numbers.append(number * 2)
+#   print(new_numbers)
+def doubleNum(l):
+	return list(map(lambda l1:l1*2,l))
+
