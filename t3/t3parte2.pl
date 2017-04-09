bdd0762508787844782be025489ae212e7a1ed43
fedps <felipe.souza@ecomp.ufsm.br>
2017-04-09 10:12:14 -0300

    Update t3parte2.pl

--- t3/t3parte2.pl
+++ t3/t3parte2.pl
@@ -16,7 +16,8 @@ somaQuad(X,Y,Q) :- Q is X^2+Y^2.
 zeroInit(L) :- nth0(0,L,0).
 
 
-%3. Defina um predicado hasEqHeads(L1,L2) que seja verdadeiro se as listas L1 %  e L2 possuírem o mesmo primeiro elemento. Exemplos de uso:
+%3. Defina um predicado hasEqHeads(L1,L2) que seja verdadeiro se as listas L1 %  e L2 possuírem o mesmo primeiro elemento. 
+%   Exemplos de uso:
 %  ?- hasEqHeads([a,b,c],[b,c,a]).
 %     false.
 %  ?- hasEqHeads([a,b,c],[a,c,a]).
@@ -24,7 +25,8 @@ zeroInit(L) :- nth0(0,L,0).
 hasEqHeads(L1,L2) :- L1 = [H|_], L2 = [G|_], H = G.
 
 
-%4. Defina um predicado has5(L) que seja verdadeiro se L for uma lista de 5 elementos. Lembre de como funciona a unificação em Prolog e resolva este exercício 
+%4. Defina um predicado has5(L) que seja verdadeiro se L for uma lista de 5 elementos. Lembre de como funciona a 
+%   unificação em Prolog e resolva este exercício 
 %   sem usar o predicado pré-definido length. Exemplos de uso:
 %   ?- has5([a,b,c]).
 %      false.
@@ -33,7 +35,8 @@ hasEqHeads(L1,L2) :- L1 = [H|_], L2 = [G|_], H = G.
 has5(L) :- L = [_,_,_,_,_].
 
 
-%5. Defina um predicado hasN(L,N) que seja verdadeiro se L for uma lista de N elementos. Agora você pode usar um predicado pré-definido.
+%5. Defina um predicado hasN(L,N) que seja verdadeiro se L for uma lista de N elementos. Agora você pode usar um predicado 
+%   pré-definido.
 %   ?- hasN([a,b,c],3).
 %      true.
 %   ?- hasN([a,b,c,d,e],3).
@@ -59,8 +62,9 @@ isBin(L) :- L = [H|T],
             isBin(T).
 
 
-%7. Defina um predicado mesmaPosicao(A,L1,L2) para verificar se um elemento A está na mesma posição nas listas L1 e L2. Assuma que L1 e L2 sejam permutações de uma 
-%   lista de elementos distintos, sem repetições. Dica: procure auxílio em predicados pré-definidos. Exemplo de uso:
+%7. Defina um predicado mesmaPosicao(A,L1,L2) para verificar se um elemento A está na mesma posição nas listas L1 e L2. 
+%   Assuma que L1 e L2 sejam permutações de uma lista de elementos distintos, sem repetições. Dica: procure auxílio 
+%   em predicados pré-definidos. Exemplo de uso:
 %   ?­ mesmaPosicao(c,[a,b,c,d,e],[e,d,c,b,a]).
 %     true
 %   ?­ mesmaPosicao(b,[a,b,c,d,e],[e,d,c,b,a]).
@@ -68,7 +72,8 @@ isBin(L) :- L = [H|T],
 mesmaPosicao(A,L1,L2) :- nth0(X,L1,A),nth0(Y,L2,A), X=Y.
 
 
-%8. Defina um predicado repete5(E,L) que seja verdadeiro se a lista L for composta por exatamente 5 repetições do elemento E. Não use recursão. Exemplos:
+%8. Defina um predicado repete5(E,L) que seja verdadeiro se a lista L for composta por exatamente 5 repetições do elemento E. 
+%   Não use recursão. Exemplos:
 %   ?- repete5(a,L).
 %      L = [a, a, a, a, a].
 %   ?- repete5(a,[a,a]).
@@ -93,7 +98,8 @@ sumQuads(L,S) :- L = [H|T],
 		 S is H^2 + AUX.
 
 
-%10. Defina um predicado recursivo repete(N,E,L) que seja verdadeiro se a lista L for composta por exatamente N repetições do elemento E.
+%10. Defina um predicado recursivo repete(N,E,L) que seja verdadeiro se a lista L for composta por exatamente N repetições 
+%    do elemento E.
 repete(0,_,[]).
 repete(N,E,L) :- L = [H|T],
 		 length(L,N),
@@ -102,7 +108,8 @@ repete(N,E,L) :- L = [H|T],
                  repete(AUX,E,T).	         
 
 
-%11. Defina um predicado recursivo mapop(L1,L2) que seja verdadeiro se a lista L2 for formada aplicando-se a operação 2*x+1 a cada elemento x da lista L1.
+%11. Defina um predicado recursivo mapop(L1,L2) que seja verdadeiro se a lista L2 for formada aplicando-se a operação 
+%    2*x+1 a cada elemento x da lista L1.
 %    ?- mapop([1,2,3],L).
 %       L = [3, 5, 7] .
 %    ?- mapop([],L).
