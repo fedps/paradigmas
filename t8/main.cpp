fd93015f2f1ed34cf10f6fc1ae31a5d0285939e7
fedps <felipe.souza@ecomp.ufsm.br>
2017-07-01 15:53:29 -0300

    Add files via upload

--- t8/main.cpp
+++ t8/main.cpp
@@ -10,11 +10,11 @@
 #include "window.h"
 
 
-std::string genHtmlMark(std::string str, std::string mark){
+std::string genHtmlMark(std::string str, std::string mark){ //função auxiliar para adicionar código html
   return "<"+mark+">"+str+"</"+mark+">";
 }
 
-std::string genHtmlGroup(std::string id,std::string label){
+std::string genHtmlGroup(std::string id,std::string label){ //função auxiliar para adicionar código html
   return"<div class=\"form-group\">\n<label for=\""+id+"\" class=\"col-sm-2 control-label\">"+label+          
         "</label>\n<div class=\"col-sm-6\">\n<input type=\"text\" class=\"form-control validate[required]\" id=\""+
         id+"\" placeholder=\""+label+"\">\n</div>\n</div>\n";
@@ -68,7 +68,7 @@ public:
     w->show();
   }
   
-  static void cbBtnImport(Fl_Widget* btn, void* userdata) {
+  static void cbBtnImport(Fl_Widget* btn, void* userdata) { //importa dados do csv
     View* gui = static_cast<View*>(userdata);
     std::string inputIn(gui->inputIn->value());
     std::ifstream file(inputIn);
@@ -76,13 +76,13 @@ public:
     std::string cell[3];
     bool firstLine = true;
     while (std::getline(file, line)) {
-      if(firstLine){
+      if(firstLine){ //se for a primeira linha, lê o título da aplicação
         std::stringstream linestream(line);  
         std::getline(linestream, cell[0]);
         gui->inputTitle->value(cell[0].c_str());
         firstLine = false;
       }
-      else{
+      else{ //se não for a primeira linha, obtém os dados normalmente separado por vírgula
         std::stringstream linestream(line);
         std::getline(linestream, cell[0], ',');
         std::getline(linestream, cell[1], ',');
@@ -90,16 +90,16 @@ public:
         gui->browser->add((cell[0]+ " : " +cell[1]).c_str());}
     }
   }
-  static void cbBtnExport(Fl_Widget* btn, void* userdata) {
+  static void cbBtnExport(Fl_Widget* btn, void* userdata) { //exporta os dados do browser pro .csv
     View* gui = static_cast<View*>(userdata);
     std::string inputOut(gui->inputOut->value());
     std::ofstream file (inputOut);
     if(file.is_open()){
-      file<<gui->inputTitle->value();
+      file<<gui->inputTitle->value(); //adiciona o título da aplicação na primeira linha
       file<<"\n";
       int linhas = gui->browser->size();
       int index=1;
-      while (index <= linhas) {
+      while (index <= linhas) { //loop que adiciona os dados
         file<<gui->data[index-1].getId().c_str();
         file<<",";
         file<<gui->data[index-1].getLabel().c_str();
@@ -166,7 +166,7 @@ public:
       gui->browser->remove(index);
     }
   }
-  static void cbBtnGenerate(Fl_Widget* btn, void* userdata) {
+  static void cbBtnGenerate(Fl_Widget* btn, void* userdata) { //Função que gera o código html
     View* gui = static_cast<View*>(userdata);
     std::ifstream file("template.html");
     std::ofstream fileOut ("index.html");
@@ -182,21 +182,21 @@ public:
         } else if(line == ">>>2<<<"){
           fileOut<<genHtmlMark(inputTitle,"h1");
         } else if(line == ">>>3<<<"){
-          int size = gui->browser->size();
+          int size = gui->browser->size(); //pega a quantidade de itens no browser
           int index=1;
-          while (index <= size) {
+          while (index <= size) { //loop que pega os ids do browser do GUI
             fileOut<<genHtmlGroup(gui->data[index-1].getId().c_str(),gui->data[index-1].getLabel().c_str());
             index++;
           }
         } else if(line == ">>>4<<<"){
           int size = gui->browser->size();
           int index=1;
-          fileOut<<"dataSet[seq]=[index, icons,";
-          while (index <= size) {
+          fileOut<<"dataSet[seq]=[index, icons,"; //adiciona primeira parte
+          while (index <= size) { //loop que pega os ids do browser do GUI
             std::string inputId(gui->data[index-1].getId().c_str());
             fileOut<<"object."+inputId;
             index++;
-            if(index<=size)
+            if(index<=size) //se não for o último dado, adiciona vírgula
               fileOut<<", ";
           }
           fileOut<<"];\n";
@@ -204,7 +204,7 @@ public:
           int size = gui->browser->size();
           int index=1;
           fileOut<<"oTable=$(\'#table\').dataTable({\n\"data\": dataSet,\n\"columns\": [\n{ \"title\": \"Seq\", \"class\": \"center\" },\n{ \"title\": \"\", \"class\": \"center\" },\n";
-          while (index <= size) {
+          while (index <= size) { //loop que pega os labels do browser do GUI
             std::string inputLabel(gui->data[index-1].getLabel().c_str());          
             fileOut<<"{ \"title\": \""+inputLabel+"\"},\n";
             index++;
@@ -214,7 +214,7 @@ public:
           int size = gui->browser->size();
           int index=1;
           fileOut<<"$('#table tbody').on( 'click', '.glyphicon-edit', function () {\n$('#list').hide();\nvar rows = $(this).parents('tr').children();\n$('#seq').text(rows[0].innerHTML);\n";
-          while (index <= size) {
+          while (index <= size) { //loop que pega os ids do browser do GUI
             std::string inputId(gui->data[index-1].getId().c_str());
             index++;
             fileOut<<"$('#"+inputId+"').val(rows["+std::to_string(index)+"].innerHTML);\n";            
@@ -224,7 +224,7 @@ public:
           int size = gui->browser->size();
           int index=1;
           fileOut<<"function save() {\n  var seq=$('#seq').text();\n  if ( isNaN(seq) ) { seq=0; }\n  var key=formId+\"_\"+seq;\n  var object={\n";
-          while (index <= size) {
+          while (index <= size) { //loop que pega os ids do browser do GUI
             std::string inputId(gui->data[index-1].getId().c_str());
             fileOut<<"\""+inputId+"\":$(\"#"+inputId+"\").val(),\n";
             index++;
